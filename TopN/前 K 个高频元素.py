3905b36493411f536c97130d68d85c6992599952
xiangyan <yandun0702@gmail.com>
2023-06-28 14:53:44 +0800

    topk_

--- TopN/前 K 个高频元素.py
+++ TopN/前 K 个高频元素.py
@@ -7,3 +7,34 @@
 输入: nums = [1], k = 1
 输出: [1]
 '''
+from heap import MaxHeap
+from collections import Counter,defaultdict
+import random
+random.seed(42)
+K = 2
+data = [1,1,1,2,2,3]
+
+def func():
+    myheap = MaxHeap() #最大堆
+
+    counts = Counter(data)
+    counts2 = defaultdict(list)
+    for key,value in counts.items():
+        counts2[value].append(key) 
+    
+    for key,value in counts2.items():
+        myheap.insert((key,tuple(value)))
+
+    print(myheap.heap)
+
+    res = []
+    for i in range(len(counts2)):
+        _ = myheap.pop()
+        print(i,_)
+        res.extend(list(_[1]))
+        if len(res) >= K:
+            break
+        
+    return res[0:K]
+
+print('res = ',func())
